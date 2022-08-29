c3e59a75c9927609c241e8a8b8b3cf228ea8abd9
Philippine <philippine.hedin@hotmail.fr>
2022-08-29 14:47:35 +0200

    Added a new carroussel

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-let slideIndex = 1;
-let slides = document.getElementsByClassName("mySlides");
-let dots = document.getElementsByClassName("dot");
+let slideIndex = [1,1]; 
+let slidesCarroussels = [
+  document.getElementsByClassName("slide-carroussel1"),
+  document.getElementsByClassName("slide-carroussel2")
+];
+let dots = [
+  document.getElementsByClassName("dot-carroussel1"),
+  document.getElementsByClassName("dot-carroussel2")
+]; 
 
-function showSlides(n) {
-  if (n > slides.length) {
-    slideIndex = 1
+function showSlides(n, carrousselIndex) {
+  if (n > slidesCarroussels[carrousselIndex].length) {
+    slideIndex[carrousselIndex] = 1; 
   }    
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex[carrousselIndex] = slidesCarroussels[carrousselIndex].length
   }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (let i = 0; i < slidesCarroussels[carrousselIndex].length; i++) {
+    slidesCarroussels[carrousselIndex][i].style.display = "none";
   }
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (let i = 0; i < dots[carrousselIndex].length; i++) {
+    dots[carrousselIndex][i].className = dots[carrousselIndex][i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slidesCarroussels[carrousselIndex][slideIndex[carrousselIndex] - 1].style.display = "block";  
+  dots[carrousselIndex][slideIndex[carrousselIndex] - 1].className += " active";
 }
 
-function plusSlides(n) {
-  slideIndex += n;
-  showSlides(slideIndex);
+function plusSlides(n, carrousselIndex) {
+  slideIndex[carrousselIndex] += n;
+  showSlides(slideIndex[carrousselIndex], carrousselIndex);
 }
 
-function currentSlide(n) {
-  slideIndex = n
-  showSlides(slideIndex);
+function currentSlide(n, carrousselIndex) {
+  slideIndex[carrousselIndex] = n
+  showSlides(slideIndex[carrousselIndex], carrousselIndex);
 }
 
-showSlides(slideIndex);
+showSlides(1, 0);
+showSlides(1, 1);
 
 // HAMBURGER MENU 
 function myFunction(id) {
